6717faf47987d87e4656d4fca1dd6fe158fcfa28
Rosbi <50968286+Rosbi@users.noreply.github.com>
2019-12-16 13:53:54 -0300

    Merge T4 x T5

--- Trabalho5/src/cidade.c
+++ Trabalho5/src/cidade.c
@@ -7,9 +7,6 @@
 #include "leitura.h"
 #include "quadra.h"
 
-extern double svgXMax;
-extern double svgYMax;
-
 typedef struct city
 {
     // Tree arvoreCirculo;
@@ -700,8 +697,8 @@ void qry_bombaRadiacao(double x, double y, char* nomeSvg, Tree retas, char* colo
     int raio = distanciaPontosD(0, 0, svgXMax, svgYMax), quantRaios=1000, i=0;
     Reta linhaAtual = criarReta(x, y, svgXMax+100, svgYMax+100);
     Ponto* pontos = malloc(retaSizeof()*quantRaios);
-    Reta retaSvgXMin=criarReta(0, 0, 0, svgYMax), retaSvgYMin=criarReta(0, 0, svgXMax, 0);
-    Reta retaSvgXMax=criarReta(svgXMax, 0, svgXMax, svgYMax), retaSvgYMax=criarReta(0, svgYMax, svgXMax, svgYMax);
+    Reta retaSvgXMin=criarReta(-1, -1, -1, svgYMax), retaSvgYMin=criarReta(-1, -1, svgXMax, -1);
+    Reta retaSvgXMax=criarReta(svgXMax, -1, svgXMax, svgYMax), retaSvgYMax=criarReta(-1, svgYMax, svgXMax, svgYMax);
 
     if(x>svgXMax || y>svgYMax){
         if(x>svgXMax && y>svgYMax){
